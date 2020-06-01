7594f6c987ad52d85c8c4befa64b95413e745bba
Ashley Sands <ashsands1@amstrike.com>
2020-06-01 02:20:54 +0100

    add ResizeWindow Method

--- Source/js/objects/HUD.js
+++ Source/js/objects/HUD.js
@@ -13,6 +13,17 @@ class HUD extends Transform
         super(posX, posY, scaleX, scaleY, rotation);
         this.gameManager = gameManager;
 
+        this.uiRect = new Rect(0, 0, this.gameManager.mapSize.x, 2);
+        this.uiRectTime = new Rect(this.gameManager.mapSize.x - 4 , -0.25, 5, 2);
+        this.uiRectMines = new Rect(0.5, -0.25, 5, 2);
+        this.uiRectCells = new Rect(0.5, 0.5, 5, 2);
+
+    }
+
+    ResizeWindow(width, height)
+    {
+        this.uiRect.scale.x =  width;
+        this.uiRectTime.position.x = width.x;
     }
 
     DrawObject( canvasRenderer )
@@ -22,16 +33,12 @@ class HUD extends Transform
          */
 
         // TODO: this needs to go into its own function,
-        var uiRect = new Rect(0, 0, this.gameManager.mapSize.x, 2);
-        var uiRectTime = new Rect(this.gameManager.mapSize.x - 4 , -0.25, this.gameManager.mapSize.x, 2);
-        var uiRectMines = new Rect(0.5, -0.25, this.gameManager.mapSize.x, 2);
-        var uiRectCells = new Rect(0.5, 0.5, this.gameManager.mapSize.x, 2);
+        
+        canvasRenderer.DrawRect( this.uiRect, "white", "black", "3" );
 
-        canvasRenderer.DrawRect( uiRect, "white", "black", "3" );
-
-        canvasRenderer.DrawText( uiRectTime, `Time: ${this.gameManager.GetTimeString()}`, "black", "16px");
-        canvasRenderer.DrawText( uiRectMines, `Mines: ${this.gameManager.mineCount}` , "black", "16px");
-        canvasRenderer.DrawText( uiRectCells, `Remaining cells: ${this.gameManager.remainingTiles}`, "black", "16px");
+        canvasRenderer.DrawText( this.uiRectTime, `Time: ${this.gameManager.GetTimeString()}`, "black", "16px");
+        canvasRenderer.DrawText( this.uiRectMines, `Mines: ${this.gameManager.mineCount}` , "black", "16px");
+        canvasRenderer.DrawText( this.uiRectCells, `Remaining cells: ${this.gameManager.remainingTiles}`, "black", "16px");
 
     }
 
